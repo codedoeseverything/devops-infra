c47d7e9a23586cf5c4cdb04ed1bd9fc509063e7b
codedoeseverything <codedoeseverything@gmail.com>
2021-02-22 10:23:01 +1100

    fix-25

--- scripts/ec2-key-pair.sh
+++ scripts/ec2-key-pair.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-aws ec2 describe-key-pairs --key-name $EC2_KEY_PAIR --output text --region $REGION | grep $EC2_KEY_PAIR &> /dev/null
+aws ec2 describe-key-pairs --key-name $EC2KEYPAIR --output text --region $REGION | grep $EC2KEYPAIR &> /dev/null
 if [ $? == 0 ]
 then
-  echo $EC2_KEY_PAIR "Key pair already exist hence no need to re-create again"
+  echo $EC2KEYPAIR "Key pair already exist hence no need to re-create again"
 else
-  aws ec2 create-key-pair --key-name $EC2_KEY_PAIR --query 'KeyMaterial' --output text > $EC2_KEY_PAIR.pem
-  aws s3 cp $EC2_KEY_PAIR.pem s3://$S3BucketNameKeyPair/$STACK_NAME/ec2-keys/ --acl bucket-owner-full-control
-  echo $EC2_KEY_PAIR "successfully created and copied to centralized S3 bucket"
-  rm $EC2_KEY_PAIR.pem
+  aws ec2 create-key-pair --key-name $EC2KEYPAIR --query 'KeyMaterial' --output text > $EC2KEYPAIR.pem
+  aws s3 cp $EC2KEYPAIR.pem s3://$S3BucketNameKeyPair/$STACK_NAME/ec2-keys/ --acl bucket-owner-full-control
+  echo $EC2KEYPAIR "successfully created and copied to centralized S3 bucket"
+  rm $EC2KEYPAIR.pem
 fi
