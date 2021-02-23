b25ab43af39826a59107e00d2997f4550e70dcea
codedoeseverything <codedoeseverything@gmail.com>
2021-02-24 00:23:20 +1100

    fix-98

--- scripts/cfn-doc-generator.sh
+++ scripts/cfn-doc-generator.sh
@@ -6,5 +6,6 @@ cd cfn-templates
 CFN_TEMPLATES=$(ls *.yml)
 for i in $CFN_TEMPLATES ; do
   echo "Generating cloudformation docs for file name "$i
-  python3 scripts/cfn-docs-generator.py $i >> docs/$i.html
+  output=$(echo "$i" | cut -f 1 -d '.')
+  python3 scripts/cfn-docs-generator.py $i >> ../docs/output.html
 done
