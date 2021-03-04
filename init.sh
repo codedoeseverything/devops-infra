d8a8023523309832cc5e481000f2050218602e53
codedoeseverything <codedoeseverything@gmail.com>
2021-03-05 10:01:16 +1100

    fix-101

--- init.sh
+++ init.sh
@@ -18,11 +18,25 @@ for i in $CFN_TEMPLATES ; do
 done
 
 
-mkdir -p docs
+mkdir -p ../docs/cfn-param-info
 echo "Run cloudformation docs generator"
 CFN_TEMPLATES=$(ls *.yml)
 for i in $CFN_TEMPLATES ; do
   echo "Generating cloudformation docs for file name "$i
   output=$(echo "$i" | cut -f 1 -d '.')
-  python3 ../scripts/cfn-docs-generator.py $i >> ../docs/$output.html
+  python3 ../scripts/cfn-docs-generator.py $i >> ../docs/cfn-param-info/$output.html
+done
+
+
+mkdir -p ../images/graph
+cp *.yml ../images/graph
+cd ../images/graph
+echo "Run cloudformation dot graph flow diagram into png format image generator"
+CFN_TEMPLATES=$(ls *.yml)
+for i in $CFN_TEMPLATES ; do
+  echo "Generating dot graph flow diagram for file name "$i
+  output=$(echo "$i" | cut -f 1 -d '.')
+  cfn-lint $i -g
+  dot $i.dot -Tpng -o $output.png
+  rm $i $i.dot
 done
