15666b354fb5d4ef5964f9adef7223ccd43dd9bb
codedoeseverything <codedoeseverything@gmail.com>
2021-03-05 20:55:32 +1100

    fix-16

--- scripts/cfn-create-update.py
+++ scripts/cfn-create-update.py
@@ -29,6 +29,7 @@ def main(stack_name, template, parameters, tags):
         'Parameters': parameter_data,
         'Tags': tag_data,
         'Capabilities': ["CAPABILITY_NAMED_IAM"],
+        'OnFailure': 'DO_NOTHING',
     }
 
     try:
