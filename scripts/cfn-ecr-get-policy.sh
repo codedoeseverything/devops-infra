ea8e422bae4b3a03898c81434470e52b61e2259e
codedoeseverything <codedoeseverything@gmail.com>
2021-03-26 14:04:11 +1100

    13

--- scripts/cfn-ecr-get-policy.sh
+++ scripts/cfn-ecr-get-policy.sh
@@ -1,6 +1,18 @@
 #!/usr/bin/env bash
 
+NAME=AllowECRAccess-$STACK_NAME-$ENV
+
 aws ecr get-repository-policy \
     --repository-name $ECR_REPONAME | \
-    jq -r '.policyText'> current-iam-policy.json
+    grep $NAME &> /dev/null
 
+if [ $? == 0 ]
+then
+    echo $NAME "ECR IAM Policy already exist hence no need to re-create again"
+else
+    aws ecr get-repository-policy \
+    --repository-name $ECR_REPONAME | \
+    jq -r '.policyText'> current-iam-policy.json
+fi
+
+python3 --version
