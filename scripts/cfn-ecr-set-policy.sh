e0e6556100efec1b4e3272cac4ea7198c0527c56
codedoeseverything <codedoeseverything@gmail.com>
2021-03-26 13:42:06 +1100

    10

--- scripts/cfn-ecr-set-policy.sh
+++ scripts/cfn-ecr-set-policy.sh
@@ -7,8 +7,8 @@ sed -i "s/NAME/$NAME/g" current-iam-policy.json
 sed -i "s/CURRENT/$CURRENTACCOUNTID/g" current-iam-policy.json
 cat current-iam-policy.json
 
-# aws ecr set-repository-policy \
-#     --repository-name $ECR_REPONAME \
-#     --policy-text file://current-iam-policy.json >> /dev/null
+aws ecr set-repository-policy \
+    --repository-name $ECR_REPONAME \
+    --policy-text file://current-iam-policy.json >> /dev/null
 
 
