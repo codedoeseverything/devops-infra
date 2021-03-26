63001a1e7595ce3328017645ae0d137d6c7385a3
codedoeseverything <codedoeseverything@gmail.com>
2021-03-26 13:44:44 +1100

    11

--- scripts/cfn-ecr-get-policy.sh
+++ scripts/cfn-ecr-get-policy.sh
@@ -4,5 +4,3 @@ aws ecr get-repository-policy \
     --repository-name $ECR_REPONAME | \
     jq -r '.policyText'> current-iam-policy.json
 
-
-
--- scripts/cfn-ecr-set-policy.sh
+++ scripts/cfn-ecr-set-policy.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
 
-NAME=$STACK_NAME-$ENV
+NAME=AllowECRAccess-$STACK_NAME-$ENV
 
-cat current-iam-policy.json
 sed -i "s/NAME/$NAME/g" current-iam-policy.json
 sed -i "s/CURRENT/$CURRENTACCOUNTID/g" current-iam-policy.json
-cat current-iam-policy.json
+
 
 aws ecr set-repository-policy \
     --repository-name $ECR_REPONAME \
