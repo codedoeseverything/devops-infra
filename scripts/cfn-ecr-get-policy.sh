be63daccc948e56e1b1fdf9e408bfaf607eb6497
codedoeseverything <codedoeseverything@gmail.com>
2021-03-26 14:06:52 +1100

    14

--- scripts/cfn-ecr-get-policy.sh
+++ scripts/cfn-ecr-get-policy.sh
@@ -13,6 +13,16 @@ else
     aws ecr get-repository-policy \
     --repository-name $ECR_REPONAME | \
     jq -r '.policyText'> current-iam-policy.json
+
+    python3 scripts/cfn-ecr-policy.py
+
+    sed -i "s/NAME/$NAME/g" current-iam-policy.json
+    sed -i "s/CURRENT/$CURRENTACCOUNTID/g" current-iam-policy.json
+
+    aws ecr set-repository-policy \
+        --repository-name $ECR_REPONAME \
+        --policy-text file://current-iam-policy.json >> /dev/null
+
 fi
 
-python3 --version
+
